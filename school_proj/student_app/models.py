1a4e2a52e4c11e938a677cd57a6b6da3cad8426b
dagaither <gaither@protonmail.com>
2024-03-19 16:39:55 -0400

    10/10 test cases passing

--- school_proj/student_app/models.py
+++ school_proj/student_app/models.py
@@ -1,10 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
 class Student(models.Model):
-    name = models.CharField(max_length=255, null = False, blank = False)
-    student_email = models.EmailField(null = False, blank = False)
-    personal_email = models.EmailField(null = False, blank = False)
-    locker_number = models.IntegerField(default=1, null = False, blank = False)
-    locker_combination = models.CharField(max_length=255, null = False, blank = False)
-    good_student = models.BooleanField()
+    name = models.CharField(max_length=50)
+    student_email = models.EmailField(max_length=100, null=True, blank=True, unique=True)
+    personal_email = models.EmailField(max_length=100, null=True, blank=True, unique=True)
+    locker_number = models.IntegerField(default=110, unique=True)
+    locker_combination = models.CharField(max_length=8, default='12-12-12')
+    good_student = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.student_email} - {self.locker_number}"
+
+    def locker_reassignment(self, num):
+        self.locker_number = num
+        self.save()
+
+    def student_status(self, param: bool):
+        self.good_student = param
+        self.save()
